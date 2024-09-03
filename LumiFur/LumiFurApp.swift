b79f3cfb2f1098d0080a03db6553073760a1b34f
Stef1949 <stef1949.sr@gmail.com>
2024-09-03 13:35:11 +0100

    Initial Commit

---
+++ LumiFur/LumiFurApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiFurApp.swift
+//  LumiFur
+//
+//  Created by Stephan Ritchie on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LumiFurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
